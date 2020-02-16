9696375c4f868ccfb88f50bcfe2b9639dcebefeb
Mark Tiddy <marktiddy@gmail.com>
2020-02-16 17:49:55 +0000

    Updates

---
+++ precache-manifest.3d93e558fb687eff5bb682a59dd107d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0fdd79319136d59cfdf621e7a95c3eb",
+    "url": "/hangout/index.html"
+  },
+  {
+    "revision": "b80ba8f347c338891a64",
+    "url": "/hangout/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d468a09817313c7c1964",
+    "url": "/hangout/static/js/2.684a1e47.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hangout/static/js/2.684a1e47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b80ba8f347c338891a64",
+    "url": "/hangout/static/js/main.83ac7f7b.chunk.js"
+  },
+  {
+    "revision": "dbc13f41576c1660f200",
+    "url": "/hangout/static/js/runtime-main.49fbb017.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hangout/static/media/logo.5d5d9eef.svg"
+  }
+]);
