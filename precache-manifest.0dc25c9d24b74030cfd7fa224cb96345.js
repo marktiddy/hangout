3b4d270c9a8c5fc3974031783cd29e48b4f6ecd3
Mark Tiddy <marktiddy@gmail.com>
2020-02-25 21:40:57 +0000

    Updates

--- precache-manifest.0dc25c9d24b74030cfd7fa224cb96345.js
+++ precache-manifest.0dc25c9d24b74030cfd7fa224cb96345.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4cf41df1bb129724ce23581836f4ada9",
+    "revision": "8bf0fd16c087b3ad4e38d38dee748102",
     "url": "/hangout/index.html"
   },
   {
-    "revision": "10ac9ee72de1afeb7876",
+    "revision": "6572f30dd24cb7f9722f",
     "url": "/hangout/static/css/main.36e68969.chunk.css"
   },
   {
-    "revision": "eeb36302616a818a6140",
-    "url": "/hangout/static/js/2.5642d625.chunk.js"
+    "revision": "6929dda93c78eb74097f",
+    "url": "/hangout/static/js/2.d775dcfd.chunk.js"
   },
   {
     "revision": "f32f3f2739f945f6e8e72f406ee3a70f",
-    "url": "/hangout/static/js/2.5642d625.chunk.js.LICENSE.txt"
+    "url": "/hangout/static/js/2.d775dcfd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "10ac9ee72de1afeb7876",
-    "url": "/hangout/static/js/main.d4b3226f.chunk.js"
+    "revision": "6572f30dd24cb7f9722f",
+    "url": "/hangout/static/js/main.477f4da7.chunk.js"
   },
   {
     "revision": "dbc13f41576c1660f200",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hangout/precache-manifest.1b1802ed88e85ad5fa55aa9e824a8ea4.js"
+  "/hangout/precache-manifest.0dc25c9d24b74030cfd7fa224cb96345.js"
 );
 
 self.addEventListener('message', (event) => {
