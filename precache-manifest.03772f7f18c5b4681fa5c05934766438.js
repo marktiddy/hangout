b1a5f3e72977404a899d3c4c3c4d543e9f5794a2
Mark Tiddy <marktiddy@gmail.com>
2020-02-25 20:25:43 +0000

    Updates

---
+++ precache-manifest.03772f7f18c5b4681fa5c05934766438.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a07e3064828a7586df313d7a4beffc",
+    "url": "/hangout/index.html"
+  },
+  {
+    "revision": "9c6d825a86492e0775f0",
+    "url": "/hangout/static/css/main.36e68969.chunk.css"
+  },
+  {
+    "revision": "eeb36302616a818a6140",
+    "url": "/hangout/static/js/2.5642d625.chunk.js"
+  },
+  {
+    "revision": "f32f3f2739f945f6e8e72f406ee3a70f",
+    "url": "/hangout/static/js/2.5642d625.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c6d825a86492e0775f0",
+    "url": "/hangout/static/js/main.7264cbc3.chunk.js"
+  },
+  {
+    "revision": "dbc13f41576c1660f200",
+    "url": "/hangout/static/js/runtime-main.49fbb017.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hangout/precache-manifest.3d93e558fb687eff5bb682a59dd107d1.js"
+  "/hangout/precache-manifest.03772f7f18c5b4681fa5c05934766438.js"
 );
 
 self.addEventListener('message', (event) => {
